2bc89ded07765c66919088a017869f4366ea7159
Jonathan "Duke" Leto <jonathan@leto.net>
2017-10-16 01:04:41 -0700

    Add an alienfile and module stub

---
+++ lib/Alien/Hush.pm
@@ -0,0 +1,13 @@
+package Alien::Hush;
+
+use strict;
+use warnings;
+use base qw( Alien::Base );
+
+# ABSTRACT: Discover or download and install Hush
+# # VERSION
+# # ALIEN SYNOPSIS
+# # ALIEN DESCRIPTION
+# # ALIEN SEE ALSO
+#
+# 1;
