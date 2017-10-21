ef9a50e751d7e98e137cba5f239f2f6c3af1ece6
Jonathan "Duke" Leto <jonathan@leto.net>
2017-10-21 01:20:08 -0700

    Add some much-needed POD so that things render nicely on the web

--- lib/Alien/Hush.pm
+++ lib/Alien/Hush.pm
@@ -4,10 +4,50 @@ use warnings;
 use base qw( Alien::Base );
 our $VERSION = 0.8888;
 
-# ABSTRACT: Discover or download and install Hush
-# # ALIEN SYNOPSIS
-# # ALIEN DESCRIPTION
-# # ALIEN SEE ALSO
-#
+=head1 NAME
 
-8888;
+Alien::Hush - Easily find or install the Hush cryptocoin full node
+
+=head1 DESCRIPTION
+
+Hush is a privacy coin that is the grandchild of Bitcoin and the
+daughter of Zcash. In addition to *transparent* taddr's from the
+Bitcoin world, there is now *shielded* addresses called zaddr's,
+which are protected by Zero Knowledge mathematics known as zkSNARKs.
+
+Learn more at the official Hush website: https://myhush.org
+
+=head1 SYNOPSIS
+
+Coming Soon
+
+=head1 AUTHOR
+
+Duke Leto <duke@leto.net>
+
+=head1 SUPPORT THIS WORK
+
+If you would like to support this work, because it helped you out
+and your life is now amazing, feel free to send some donations to
+the developer via the Hush blockchain :
+
+HUSH: zcB3LvCwYPdvVmNmTvEFdFGwb4teocrPnVD8wvBgMn8ZKo5Vq9ypJLpAuqUM31hEbBnSHCjCpax5mjUi2HEM6NB8x7vfKrV
+
+Cryptocoins make the world go round and round.
+
+=head1 SEE ALSO
+
+Bitcoin, Zcash, The Future
+
+=head1 COPYRIGHT
+
+Copyright (c) 2017 by Duke Leto <duke@leto.net>.  All rights reserved.
+
+=head1 LICENSE AGREEMENT
+
+This package is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself, Fuck Yeah.
+
+=cut
+
+8888.8888;
